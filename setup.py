4bfb43389027d13fb0dde1ac028030f516356234
John Maguire <contact@johnmaguire.me>
2016-01-07 00:35:06 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(name='pplugins',
+      version='0.1.0',
+      description='Process-based plugin framework',
+      author='John Maguire',
+      author_email='contact@johnmaguire.me',
+      url='https://github.com/JohnMaguire/pplugins',
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Software Development :: Libraries :: Application Frameworks',
+      ],
+      py_modules=['pplugins']
+      )
