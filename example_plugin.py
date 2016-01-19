ba3bcd737f5c1c0e0225cc7144dd55c97363773b
John Maguire <jmaguire@barracuda.com>
2016-01-18 19:49:07 -0500

    Plugin should call parent constructor last

--- example_plugin.py
+++ example_plugin.py
@@ -8,9 +8,9 @@ class ExamplePlugin(Plugin):
     """Don't do anything special"""
 
     def __init__(self, *args, **kwargs):
-        super(ExamplePlugin, self).__init__(*args, **kwargs)
-
         self.logger = logging.getLogger(__name__)
+
+        super(ExamplePlugin, self).__init__(*args, **kwargs)
 
     def run(self):
         self.logger.info("Child, started!")
@@ -19,8 +19,8 @@ class ExamplePlugin(Plugin):
             try:
                 event = self.interface.get_event(block=False)
             except Queue.Empty:
-                self.logger.info("Child, no message. Waiting 1 second")
-                time.sleep(1)
+                self.logger.info("Child, no message. Waiting .1 second")
+                time.sleep(.1)
                 continue
 
             if event is None:
