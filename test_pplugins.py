a3f134d9c156797d78f50005efcf555de65aa7f4
John Maguire <contact@johnmaguire.me>
2016-01-19 21:53:07 -0500

    Add tests for PluginRunner._is_plugin() and _find_plugin()

--- test_pplugins.py
+++ test_pplugins.py
@@ -1,3 +1,4 @@
+import inspect
 import Queue
 
 import pytest
@@ -35,6 +36,7 @@ def test_pluginerror_str():
         raise pplugins.PluginError("Generic error", "test")
     except pplugins.PluginError as e:
         assert str(e) == "Generic error (plugin: test)"
+
 
 def test_plugininterface_add_message():
     msg = "TEST"
@@ -96,3 +98,42 @@ def test_pluginrunner_constructor():
 
     # multiprocessing daemon flag
     assert pr.daemon is True
+
+
+@patch.multiple(pplugins.PluginRunner, __abstractmethods__=set())
+def test_pluginrunner__is_plugin():
+    class Plugin(pplugins.Plugin):
+        pass
+
+    pr = pplugins.PluginRunner(None, None, None, None)
+
+    assert pr._is_plugin(pplugins.Plugin) is False
+    assert pr._is_plugin(Plugin) is True
+
+
+@patch.multiple(pplugins.PluginRunner, __abstractmethods__=set())
+def test_pluginrunner__find_plugin_no_plugin_exception():
+    with pytest.raises(pplugins.PluginError) as excinfo:
+        pr = pplugins.PluginRunner('test', None, None, None)
+        with patch.object(pplugins.PluginRunner, '_load_plugin',
+                          return_value=None):
+            pr._find_plugin()
+
+    assert 'Unable to find a Plugin class' in str(excinfo.value)
+
+
+@patch.multiple(pplugins.PluginRunner, __abstractmethods__=set())
+@patch.object(inspect, 'getmembers', return_value=[(0, "ABC"), (0, "CDE")])
+@patch.object(pplugins.PluginRunner, '_load_plugin', return_value="MODULE")
+def test_pluginrunner__find_plugin(mock_load, mock_getmembers):
+    pr = pplugins.PluginRunner('test', None, None, None)
+    plugin = pr._find_plugin()
+
+    # make sure it called _load_plugin
+    mock_load.assert_called_once_with()
+
+    # verify it passed _is_plugin and the module returned from _load_plugin
+    mock_getmembers.assert_called_once_with("MODULE", pr._is_plugin)
+
+    # verify it returns the first item from getmembers
+    assert plugin == "ABC"
