b78ef7a00639e375d6d578a498cba9471f0c4bf8
John Maguire <contact@johnmaguire.me>
2016-01-24 10:51:42 -0500

    Use six.moves.queue for Py2 and Py3 support

--- test_pplugins.py
+++ test_pplugins.py
@@ -1,9 +1,9 @@
 import multiprocessing
 import threading
-import Queue
 
-import pytest
+from six.moves import queue
 from mock import patch
+import pytest
 
 import pplugins
 
@@ -185,8 +185,8 @@ def test_pluginmanager_stop_plugin(stop_plugin_mock, _):
 @patch.multiple(pplugins.PluginManager, __abstractmethods__=set())
 def test_pluginmanager_process_messages(_):
     pm = pplugins.PluginManager()
-    queue = Queue.Queue()
-    pm.plugins = {'test': {'messages': queue}}
+    q = queue.Queue()
+    pm.plugins = {'test': {'messages': q}}
 
     # empty queue
     with patch.object(pplugins.PluginManager, '_process_message',
@@ -195,7 +195,7 @@ def test_pluginmanager_process_messages(_):
     process_message_mock.assert_not_called()
 
     # with a message
-    queue.put('test message')
+    q.put('test message')
     with patch.object(pplugins.PluginManager, '_process_message',
                       return_value=None) as process_message_mock:
         pm.process_messages()
